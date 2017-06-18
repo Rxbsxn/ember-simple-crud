90dc2ad5dcf6fe52046e10e93668a888d7d0fc55
Robert Harężlak <harezlak.robert7@gmail.com>
2017-06-18 23:58:17 +0200

    shit its working

--- app/components/users/user-new/component.js
+++ app/components/users/user-new/component.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+const {
+  inject: { service },
+  Component
+} = Ember;
+
+export default Component.extend({
+  store: service(),
+
+  actions: {
+    newUser() {
+      let userName = this.get('userName');
+      this.get('store').createRecord('user', {name: userName}).save();
+
+      this.set('userName', '');
+    }
+  }
 });
